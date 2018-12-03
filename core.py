0edfbbf09089ec1e8d24401ad5d89471ecaf77e8
Cyril Bécret <cbcrt@MacBook-Pro-de-Cyril.local>
2018-12-03 14:48:54 +0100

    PEP8 Refactor

--- core.py
+++ core.py
@@ -1,193 +1,213 @@
 import random
 import attr
 
-class Character:
-    def level_up(self):
-        self._level += 1
-    def display_level(self):
-        print(self._name, "is level [", self._level, "]")
-    def alterate_strength(self, modifier):
-        self._stats._strength += modifier
-    def alterate_pv(self, amount):
-        self._pv += amount
-    def drink_potion(self):
-        self._pv = self._pvmax
-
 
 class Stuff:
-    def _show(self, itemSlot):
-        getattr(self, itemSlot).display()
-    def _equip(self, itemSlot, item):
-        if type(item) == Item and item._slot._name == itemSlot:
-            setattr(self, itemSlot, item)
-    def _remove(self, itemSlot):
-        setattr(self, itemSlot, Unequiped(slot = itemSlot))
+    def _show(self, itemslot):
+        getattr(self, itemslot).display()
 
-    def show_inventory(self):
-        for i in self._inventory:
-            show_item(self._inventory[i])
-            
-    def show_item(self, item):
+    def _equip(self, itemslot, item):
+        if type(item) == Item and item.slot.name == itemslot:
+            setattr(self, itemslot, item)
+
+    def _remove(self, itemslot):
+        setattr(self, itemslot, Unequiped(slot=itemslot))
+
+    @staticmethod
+    def show_item(item):
         print("------------------------------")
-        print(f"*** {item._name} ***")
-        print(f"    ({item._slot._name})")
-        print(f"Force        : +{item._stats._strength} [req.{item._requirements._strength}]")
-        print(f"Constitution : +{item._stats._constitution} [req.{item._requirements._constitution}]")
-        print(f"Dexterité    : +{item._stats._dexterity} [req.{item._requirements._dexterity}]")
-        print(f"Intelligence : +{item._stats._intelligence} [req.{item._requirements._intelligence}]")
+        print(f"*** {item.name} ***")
+        print(f"    ({item.slot.name})")
+        print(f"Force        : +{item.stats.strength} [req.{item.requirements.strength}]")
+        print(f"Constitution : +{item.stats.constitution} [req.{item.requirements.constitution}]")
+        print(f"Dexterité    : +{item.stats.dexterity} [req.{item.requirements.dexterity}]")
+        print(f"Intelligence : +{item.stats.intelligence} [req.{item.requirements.intelligence}]")
         print("------------------------------")
 
     def show_stuff(self):
-        self._show("_head")
-        self._show("_chest")
-        self._show("_legs")
-        self._show("_foot")
-        self._show("_leftHand")
-        self._show("_rightHand")
+        self._show("head")
+        self._show("chest")
+        self._show("legs")
+        self._show("foot")
+        self._show("left_hand")
+        self._show("right_hand")
 
     def show_head(self):
-        self._show("_head")
+        self._show("head")
+
     def equip_head(self, item):
-        self._equip("_head", item)
+        self._equip("head", item)
+
     def remove_head(self):
-        self._remove("_head")
+        self._remove("head")
 
     def show_chest(self):
-        self._show("_chest")
+        self._show("chest")
+
     def equip_chest(self, item):
-        self._equip("_chest", item)
+        self._equip("chest", item)
+
     def remove_chest(self):
-        self._remove("_chest")
+        self._remove("chest")
 
     def show_legs(self):
-        self._show("_legs")
+        self._show("legs")
+
     def equip_legs(self, item):
-        self._equip("_legs", item)
+        self._equip("legs", item)
+
     def remove_legs(self):
-        self._remove("_legs")
+        self._remove("legs")
 
     def show_foot(self):
-        self._show("_foot")
+        self._show("foot")
+
     def equip_foot(self, item):
-        self._equip("_foot", item)
+        self._equip("foot", item)
+
     def remove_foot(self):
-        self._remove("_foot")
+        self._remove("foot")
 
-    def show_leftHand(self):
-        self._show("_leftHand")
-    def equip_leftHand(self, item):
-        self._equip("_leftHand", item)
-    def remove_leftHand(self):
-        self._remove("_leftHand")
+    def show_left_hand(self):
+        self._show("left_hand")
 
-    def show_rightHand(self):
-        self._show("_rightHand")
-    def equip_rightHand(self, item):
-        self._equip("_rightHand", item)
-    def remove_rightHand(self):
-        self._remove("_rightHand")
+    def equip_left_hand(self, item):
+        self._equip("left_hand", item)
+
+    def remove_left_hand(self):
+        self._remove("left_hand")
+
+    def show_right_hand(self):
+        self._show("right_hand")
+
+    def equip_right_hand(self, item):
+        self._equip("right_hand", item)
+
+    def remove_right_hand(self):
+        self._remove("right_hand")
 
 
 @attr.s
 class Stats:
-    _strength = attr.ib(default=0)
-    _constitution = attr.ib(default=0)
-    _dexterity = attr.ib(default=0)
-    _intelligence = attr.ib(default=0)
+    strength = attr.ib(default=0)
+    constitution = attr.ib(default=0)
+    dexterity = attr.ib(default=0)
+    intelligence = attr.ib(default=0)
 
 
 @attr.s
 class Item(Stuff):
-    _name = attr.ib()
-    _slot = attr.ib()
-    _requirements = attr.ib()
-    _stats = attr.ib()
+    name = attr.ib()
+    slot = attr.ib()
+    requirements = attr.ib()
+    stats = attr.ib()
 
     def get_stats(self):
-        return self._stats
-        
+        return self.stats
+
+    @staticmethod
     def display(self):
         print("------------------------------")
-        print(f"*** {self._name} ***")
-        print(f"    ({self._slot._name})")
-        print(f"Force        : +{self._stats._strength} [req.{self._requirements._strength}]")
-        print(f"Constitution : +{self._stats._constitution} [req.{self._requirements._constitution}]")
-        print(f"Dexterité    : +{self._stats._dexterity} [req.{self._requirements._dexterity}]")
-        print(f"Intelligence : +{self._stats._intelligence} [req.{self._requirements._intelligence}]")
+        print(f"*** {self.name} ***")
+        print(f"    ({self.slot.name})")
+        print(f"Force        : +{self.stats.strength} [req.{self.requirements.strength}]")
+        print(f"Constitution : +{self.stats.constitution} [req.{self.requirements.constitution}]")
+        print(f"Dexterité    : +{self.stats.dexterity} [req.{self.requirements.dexterity}]")
+        print(f"Intelligence : +{self.stats.intelligence} [req.{self.requirements.intelligence}]")
         print("------------------------------")
-        
+
 
 @attr.s
 class Unequiped(Stuff):
-    _slot = attr.ib()
-    _name = attr.ib(default="Vide")
-    _requirements = attr.ib(default=Stats(0, 0, 0, 0))
-    _stats = attr.ib(default=Stats(0, 0, 0, 0))
+    slot = attr.ib()
+    name = attr.ib(default="Vide")
+    requirements = attr.ib(default=Stats(0, 0, 0, 0))
+    stats = attr.ib(default=Stats(0, 0, 0, 0))
 
-    def display(self):
+    @staticmethod
+    def display():
         print("------------------------------")
         print(f"*** Vide ***")
         print("------------------------------")
-        
+
     def get_stats(self):
-        return self._stats
+        return self.stats
 
 
 @attr.s
 class Slot:
-    _name = attr.ib(validator=attr.validators.in_(["_head",
-                                                   "_chest",
-                                                   "_legs",
-                                                   "_foot",
-                                                   "_leftHand",
-                                                   "_rightHand"]))
-        
-    def get_name(self):
-        return self._name
+    name = attr.ib(validator=attr.validators.in_(["head",
+                                                  "chest",
+                                                  "legs",
+                                                  "foot",
+                                                  "left_hand",
+                                                  "right_hand"]))
+
+    def getname(self):
+        return self.name
 
 
 @attr.s
-class Player(Character, Stuff):
-    _name = attr.ib()
-    _level = attr.ib(default=1)
-    _stats = attr.ib(default=Stats(0, 0, 0, 0))
-    _pvmax = attr.ib(default=100)
-    _pv = attr.ib(default=100)
-    _inventory = attr.ib(default=[])
-    _head = attr.ib(default=Unequiped(slot = "_head"))
-    _chest = attr.ib(Unequiped(slot = "_chest"))
-    _legs = attr.ib(Unequiped(slot = "_legs"))
-    _foot = attr.ib(Unequiped(slot = "_foot"))
-    _leftHand = attr.ib(Unequiped(slot = "_leftHand"))
-    _rightHand = attr.ib(Unequiped(slot = "_rightHand"))
-        
+class Player(Stuff):
+    name = attr.ib()
+    level = attr.ib(default=1)
+    stats = attr.ib(default=Stats(0, 0, 0, 0))
+    max_hp = attr.ib(default=100)
+    hp = attr.ib(default=100)
+    inventory = attr.ib(default=[])
+    head = attr.ib(default=Unequiped(slot="head"))
+    chest = attr.ib(default=Unequiped(slot="chest"))
+    legs = attr.ib(default=Unequiped(slot="legs"))
+    foot = attr.ib(default=Unequiped(slot="foot"))
+    left_hand = attr.ib(default=Unequiped(slot="left_hand"))
+    right_hand = attr.ib(default=Unequiped(slot="right_hand"))
+
+    def level_up(self):
+        self.level += 1
+
+    def display_level(self):
+        print(self.name, "is level [", self.level, "]")
+
+    def alter_strength(self, modifier):
+        self.stats.strength += modifier
+
+    def alter_hp(self, amount):
+        self.hp += amount
+
+    def drink_potion(self):
+        self.hp = self.max_hp
+
+    def show_inventory(self):
+        for i in self.inventory:
+            self.show_item(self.inventory[i])
+
     def _get_strength(self):
-        strength = self._stats._strength
-        strength += self._head._stats._strength
+        strength = self.stats.strength
+        strength += self.head.stats.strength
         return strength
-    
+
     def _get_constitution(self):
-        constitution = self._stats._constitution
-        constitution += self._head._stats._constitution
+        constitution = self.stats.constitution
+        constitution += self.head.stats.constitution
         return constitution
-    
+
     def _get_dexterity(self):
-        dexterity = self._stats._dexterity
-        dexterity += self._head._stats._dexterity
+        dexterity = self.stats.dexterity
+        dexterity += self.head.stats.dexterity
         return dexterity
-    
+
     def _get_intelligence(self):
-        intelligence = self._stats._intelligence
-        intelligence += self._head._stats._intelligence
+        intelligence = self.stats.intelligence
+        intelligence += self.head.stats.intelligence
         return intelligence
-        
+
     def display_stats(self):
         print("***************************")
-        print("--- Point de vie [", self._pv, "/", self._pvmax, "]")
-        print("Strength :          ", self._stats._strength)
-        print("Constitution :      ", self._stats._constitution)
-        print("Dexterity :         ", self._stats._dexterity)
-        print("Intelligence :      ", self._stats._intelligence)
+        print("--- Point de vie [", self.hp, "/", self.max_hp, "]")
+        print("Strength :          ", self.stats.strength)
+        print("Constitution :      ", self.stats.constitution)
+        print("Dexterity :         ", self.stats.dexterity)
+        print("Intelligence :      ", self.stats.intelligence)
         print("***************************")
 
 
@@ -198,71 +218,74 @@ class Fight:
 
     def __attrs_post_init__(self):
         self._attack()
-    
+
     def _attack(self):
-        self.monster.alterate_pv(-self.player._stats._strength)
-        if self.monster._pv > 0:
-            print(f"{self.player._name} enlève {self.player._stats._strength}PV à {self.monster._name}")
+        self.monster.alter_hp(-self.player.stats.strength)
+        if self.monster.hp > 0:
+            print(f"{self.player.name} enlève {self.player.stats.strength}hp à {self.monster.name}")
             self._defend()
         else:
-            print(f"{self.player._name} enlève {self.player._stats._strength}PV à {self.monster._name} et le tue")
+            print(f"{self.player.name} enlève {self.player.stats.strength}hp à {self.monster.name} et le tue")
             print(f"Player win")
-        
+
     def _defend(self):
-        self.player.alterate_pv(-self.monster._stats._strength)
-        if self.player._pv > 0:
-            print(f"{self.monster._name} enlève {self.monster._stats._strength}PV à {self.player._name}")
+        self.player.alter_hp(-self.monster.stats.strength)
+        if self.player.hp > 0:
+            print(f"{self.monster.name} enlève {self.monster.stats.strength}hp à {self.player.name}")
             self._attack()
         else:
-            print(f"{self.monster._name} enlève {self.monster._stats._strength}PV à {self.player._name} et le tue")
+            print(f"{self.monster.name} enlève {self.monster.stats.strength}hp à {self.player.name} et le tue")
             print(f"Monster win")
 
     def pick_loots(self):
-        numbre_loots = random.randint(0,3)
-        for i in range(numbre_loots):
+        number_loots = random.randint(0, 3)
+        for i in range(number_loots):
             self._generate_loot()
-    
-    def _generate_loot(self):
-        return Item("Heaume du débutant", Slot("_head"), Stats(12, 0, 10, 6), Stats(3, 0, 7, 0))
-    
+
+    @staticmethod
+    def _generate_loot():
+        return Item("Heaume du débutant", Slot("head"), Stats(12, 0, 10, 6), Stats(3, 0, 7, 0))
+
     def end_fight(self):
-        if self.player._stats._strength >= self.monster._stats._strength:
-            print(f"{self.player._name} win")
+        if self.player.stats.strength >= self.monster.stats.strength:
+            print(f"{self.player.name} win")
         else:
-            print(f"{self.monster._name} win")
-    
+            print(f"{self.monster.name} win")
+
 
 @attr.s
-class Monster(Character):
-    _name = attr.ib()
-    _level = attr.ib()
-    _stats = attr.ib()
-    _pv = attr.ib()
-        
+class Monster:
+    name = attr.ib()
+    level = attr.ib()
+    stats = attr.ib()
+    hp = attr.ib()
+
     def get_stats(self):
-        return self._stats
-    
+        return self.stats
+
+    def alter_hp(self, amount):
+        self.hp += amount
 
 
-beginnerHead = Item("Heaume du débutant", Slot("_head"), Stats(12, 0, 10, 6), Stats(3, 0, 7, 0))
-beginnerChest = Item("Plastron du débutant", Slot("_chest"), Stats(12, 0, 10, 6), Stats(5, 0, 12, 0))
-beginnerLegs = Item("Jambières du débutant", Slot("_legs"), Stats(12, 0, 10, 6), Stats(4, 0, 7, 0))
-beginnerFoot = Item("Sandales du débutant", Slot("_foot"), Stats(12, 0, 10, 6), Stats(3, 0, 5, 0))
-beginnerSword = Item("Epée du débutant", Slot("_rightHand"), Stats(12, 0, 10, 6), Stats(8, 0, 5, 0))
-beginnerShield = Item("Bouclier du débutant", Slot("_leftHand"), Stats(12, 0, 10, 6), Stats(0, 0, 10, 0))
+beginnerHead = Item("Heaume du débutant", Slot("head"), Stats(12, 0, 10, 6), Stats(3, 0, 7, 0))
+beginnerChest = Item("Plastron du débutant", Slot("chest"), Stats(12, 0, 10, 6), Stats(5, 0, 12, 0))
+beginnerLegs = Item("Jambières du débutant", Slot("legs"), Stats(12, 0, 10, 6), Stats(4, 0, 7, 0))
+beginnerFoot = Item("Sandales du débutant", Slot("foot"), Stats(12, 0, 10, 6), Stats(3, 0, 5, 0))
+beginnerSword = Item("Epée du débutant", Slot("right_hand"), Stats(12, 0, 10, 6), Stats(8, 0, 5, 0))
+beginnerShield = Item("Bouclier du débutant", Slot("left_hand"), Stats(12, 0, 10, 6), Stats(0, 0, 10, 0))
 
-jacky = Player("Jacky le guerrier", 1, Stats(10,12,11,13), 40)
+jacky = Player("Jacky le guerrier", 1, Stats(10, 12, 11, 13), 40)
 
 jacky.equip_head(beginnerHead)
 jacky.equip_chest(beginnerChest)
 jacky.equip_legs(beginnerLegs)
 jacky.equip_foot(beginnerFoot)
-jacky.equip_rightHand(beginnerSword)
-jacky.equip_leftHand(beginnerShield)
+jacky.equip_right_hand(beginnerSword)
+jacky.equip_left_hand(beginnerShield)
 
-gobelin1 = Monster("Toto le gobelin", 2, Stats(6,4,5,0), 36)
-gobelin2 = Monster("Gérard le grand gobelin", 2, Stats(6,4,5,0), 36)
-ogre1 = Monster("Pascal l'ogre puant'", 2, Stats(9,4,5,0), 50)
+gobelin1 = Monster("Toto le gobelin", 2, Stats(6, 4, 5, 0), 36)
+gobelin2 = Monster("Gérard le grand gobelin", 2, Stats(6, 4, 5, 0), 36)
+ogre1 = Monster("Pascal l'ogre puant'", 2, Stats(9, 4, 5, 0), 50)
 
 Fight(jacky, gobelin1)
 Fight(jacky, gobelin2)
